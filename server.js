04d27c49804d9c5c48477bc1167f48c2f034297f
vungoc98 <thingocvu98@gmail.com>
2019-04-06 15:29:28 +0700

    'login'

--- server.js
+++ server.js
@@ -15,21 +15,21 @@ res.sendFile(path.join(__dirname+'/dist/demo-deploy/index.html'));
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 // Xu ly thong tin dang nhap
-app.post('/login', jsonParser, (req, res) => {
-	// var sql = "select * from user1 where username = ? and password = ?";
+// app.post('/login', jsonParser, (req, res) => {
+// 	// var sql = "select * from user1 where username = ? and password = ?";
 	
-	// sql = mysql.format(sql, [req.body.name, req.body.password]);
+// 	// sql = mysql.format(sql, [req.body.name, req.body.password]);
 	 
-	// con.query(sql, function(err, results) {
-	// 	if (err) throw err; 
-	// 	if (results.length == 0) {
-	// 		this.login = new Login(check = 0);
-	// 	}
-	// 	for (var i = 0; i < results.length; i++) {
-	// 		this.login = new Login(1, results[i].username, results[i].password, results[i].name, results[i].address, results[i].email, results[i].mobile, results[i].acount_type);	 
-	// 	}
-	// 	res.send(this.login);
-	// })
-	res.send("hello " + req.body.name);
+// 	// con.query(sql, function(err, results) {
+// 	// 	if (err) throw err; 
+// 	// 	if (results.length == 0) {
+// 	// 		this.login = new Login(check = 0);
+// 	// 	}
+// 	// 	for (var i = 0; i < results.length; i++) {
+// 	// 		this.login = new Login(1, results[i].username, results[i].password, results[i].name, results[i].address, results[i].email, results[i].mobile, results[i].acount_type);	 
+// 	// 	}
+// 	// 	res.send(this.login);
+// 	// })
+// 	res.send("hello " + req.body.name);
    
-});
+// });
