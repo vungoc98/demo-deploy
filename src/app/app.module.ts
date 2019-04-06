9f3f012e48d40451ce7347c4d7fbcf2c4cd91657
vungoc98 <thingocvu98@gmail.com>
2019-04-06 09:16:23 +0700

    updates to deploy to heroku

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './login/login.component';
+const routesConfig: Routes = [
+  { path: '', component: LoginComponent }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
+   // AppRoutingModule
+    RouterModule.forRoot(routesConfig,{onSameUrlNavigation: 'reload'}),
     BrowserModule,
     AppRoutingModule
   ],
