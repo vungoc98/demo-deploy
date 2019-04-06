5563f8a17a83f898ce869c08cf146c7e067b982c
vungoc98 <thingocvu98@gmail.com>
2019-04-06 09:30:12 +0700

    updates to deploy to heroku

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormBuilder} from '@angular/forms';
+import { Http, Headers } from '@angular/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,86 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  formSignIn: FormGroup;
+  thongTinTaiKhoan: any;
+  results: any;
+  nha_phan_phoi;
+  data;
+  login = true;
+  info: Login;
+  @Output() onLogin: EventEmitter<any>;
+  constructor( private fb: FormBuilder, private http: Http, private router: Router) { 
+    this.onLogin = new EventEmitter();
+  }
 
   ngOnInit() {
+  	this.formSignIn = this.fb.group({
+  		name: '',
+  		password: ''
+  	})
   }
+
+  async onSubmit() { 
+    // // tao duong dan can post len
+    // const url = "http://localhost:3000/login";
+
+    // // tao header
+    // const headers = new Headers({ 'Content-Type': 'application/json' });
+
+    // // lay body gui len
+    // const body = JSON.stringify(this.formSignIn.value);
+
+    // // res la ket qua tra ve tu server
+    // await this.http.post(url, body, {headers: headers})
+    // .toPromise()
+    // .then(res => res.json())
+    // .then(resJson => {
+    //   this.thongTinTaiKhoan = resJson;
+    //   if (this.thongTinTaiKhoan.check == 0) {
+    //     alert("Tài khoản hoặc mật khẩu không đúng");
+    //   }
+       
+    //   else{ 
+    //      this.info = new Login(resJson.check, resJson.username, resJson.password, resJson.name, resJson.address, resJson.email, resJson.mobile, resJson.count_type);
+    //      this.acountInfo.setInfomation(this.info);
+    //     if (this.thongTinTaiKhoan.acount_type == "Nhà phân phối") { 
+    //       console.log(this.thongTinTaiKhoan);
+    //       this.router.navigate(['/nhaphanphoi'],{ skipLocationChange: true }); 
+    //     }
+    //     else if (this.thongTinTaiKhoan.acount_type == "Nhà cung cấp") {
+    //       this.router.navigateByUrl("/nhacungcap");
+    //     }
+    //     else if (this.thongTinTaiKhoan.acount_type == "Siêu thị") {
+    //       this.router.navigateByUrl("/sieuthi");
+    //     }
+    //   }
+        
+    // }); 
+  }
+  hienthi() {
+     //console.log(this.loginService.getThongTinTaiKhoan());
+  }
+
+}
+class Login { 
+  public check;
+  public username;
+  public password;
+  public name;
+  public address;
+  public email;
+  public mobile;
+  public acount_type;
+  constructor(check, username, password, name, address, email, mobile, acount_type) { 
+    this.check = check;
+    this.username = username;
+    this.password = password;
+    this.name = name;
+    this.address = address;
+    this.email = email;
+    this.mobile = mobile;
+    this.acount_type = acount_type;
+  }
+}
 
 }
