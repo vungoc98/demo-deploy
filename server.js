73ba4b692189ed974a0c73ef561c9e7ec9baeba5
vungoc98 <thingocvu98@gmail.com>
2019-04-14 10:48:49 +0700

    register

--- server.js
+++ server.js
@@ -9,10 +9,7 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/demo-deploy'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/demo-deploy/index.html'));
-});
+ 
 
  
 // Ket noi nodejs voi mysql
